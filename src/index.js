5ee6b6146c6dd334f9b86e5d12f97d09fccf62f2
wfx <wangfx@igancao.com>
2020-04-17 11:11:21 +0800

    创建一个react项目

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+import App from "./App.js";
+// ReactDOM.render(<App />, document.getElementById("root"));
+export {
+  App as Test1App
+}
